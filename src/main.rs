802b287700d7d81465c12f6ebe8ba6bb6c498943
Aitor <aitor.iturri@appgate.com>
2021-06-10 13:23:54 +0200

    Use AdmissionReview struct

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use log::{debug, error, info};
 use std::fmt::{Display, Formatter, Result as FResult};
 use rustls::{ServerConfig, NoClientAuth};
 use rustls::internal::pemfile::{certs, rsa_private_keys};
-use kube::api::admission::{AdmissionReview, AdmissionResponse};
+use kube::api::admission::{AdmissionReview, AdmissionResponse, AdmissionRequest};
 use json_patch::{Patch, AddOperation};
 use json_patch::PatchOperation::Add;
+use std::convert::TryInto;
 
 const APPGATE_TAG_KEY: &str = "appgate-inject";
 const APPGATE_TAG_VALUE: &str = "true";
@@ -90,18 +91,26 @@ trait AppgatePod {
     fn patch_sidecars(&self, appgate_sidecars: &AppgateSidecars) -> Result<Option<Patch>, Box<dyn Error>> {
         info!("Patching POD with appgate client");
         let mut patches = vec![];
-        let needs_patch = self.needs_patching();
-        if needs_patch && self.needs_patching() {
-            patches.insert(0,
-                           Add(AddOperation {
-                               path: "/spec/volumes".to_string(),
-                               value: serde_json::to_value(&appgate_sidecars.volumes)?,
-                           }));
-            patches.insert(0,
-                           Add(AddOperation {
-                               path: "/spec/containers".to_string(),
-                               value: serde_json::to_value(&appgate_sidecars.containers)?,
-                           }));
+        if self.needs_patching() {
+            for c in appgate_sidecars.containers.iter() {
+                patches.push(Add(AddOperation {
+                    path: "/spec/containers/-".to_string(),
+                    value: serde_json::to_value(&c)?,
+                }));
+            }
+            if self.volumes().is_some() {
+                for v in appgate_sidecars.volumes.iter() {
+                    patches.push(Add(AddOperation {
+                        path: "/spec/volumes/-".to_string(),
+                        value: serde_json::to_value(&v)?,
+                    }));
+                }
+            } else {
+                patches.extend(vec![Add(AddOperation {
+                    path: "/spec/volumes".to_string(),
+                    value: serde_json::to_value(&appgate_sidecars.volumes)?,
+                })]);
+            }
         }
         if patches.is_empty() {
             debug!("POD does not require patching");
@@ -170,16 +179,15 @@ fn load_cert_files() -> Result<(BufReader<File>, BufReader<File>), Box<dyn Error
 fn generate_patch(request_body: &str, context: &AppgateSidecars) -> Result<HttpResponse, Box<dyn Error>> {
     let admision_review = serde_json::from_str::<AdmissionReview<Pod>>(&request_body)
         .map_err(|e| format!("Error parsing payload: {}", e.to_string()))?;
-    let admission_request = admision_review.request
-        .ok_or("Admission review is not a request")?;
+    let admission_request: AdmissionRequest<Pod> = admision_review.try_into()?;
     let pod = admission_request.object.as_ref().ok_or("Admission review does not contain a POD")?;
-    debug!("Got POD request: {}", AppgatePodDisplay(&pod));
     let mut admission_response = AdmissionResponse::from(&admission_request);
     if let Some(p) = pod.patch_sidecars(&context)? {
         admission_response = admission_response.with_patch(p)?;
     }
-    let response_body = serde_json::to_string(&admission_response)?;
-    Ok(HttpResponse::Ok().body(response_body))
+    let response = &admission_response.into_review();
+    //let response_body = serde_json::to_string(&admission_response.into_review())?;
+    Ok(HttpResponse::Ok().json(&response))
 }
 
 #[post("/mutate")]
@@ -196,8 +204,9 @@ async fn mutate(request: HttpRequest, body: String) -> Result<HttpResponse, Http
 #[cfg(test)]
 mod tests {
     use k8s_openapi::api::core::v1::{Pod, Container};
-    use std::collections::BTreeMap;
-    use crate::{AppgatePod, APPGATE_SIDECAR_NAMES, load_sidecar_containers, APPGATE_VOLUME_NAMES};
+    use std::collections::{BTreeMap, HashSet};
+    use crate::{AppgatePod, APPGATE_SIDECAR_NAMES, load_sidecar_containers, AppgatePodDisplay};
+    use std::iter::FromIterator;
 
     const APPGATE_VOLUME_NAMES: [&str; 2] = ["run-appgate", "tun-device"];
 
@@ -215,8 +224,8 @@ mod tests {
         c
     }
 
-    fn run_test<F>(pod: &mut Pod, test: &TestInject, predicate: &mut F) -> (bool, String)
-        where F: FnMut(&mut Pod, &TestInject) -> (bool, String) {
+    fn run_test<F>(pod: &mut Pod, test: &TestInject, predicate: &mut F) -> (bool, String, String)
+        where F: FnMut(&mut Pod, &TestInject) -> (bool, String, String) {
         pod.metadata.labels = test.labels.as_ref()
             .map(|xs| create_labels(&xs[..]));
         let test_cs: Vec<Container> = test.containers.iter()
@@ -228,22 +237,23 @@ mod tests {
     }
 
     fn assert_tests<F>(tests: &[TestInject], predicate: &mut F) -> () where
-        F: FnMut(&mut Pod, &TestInject) -> (bool, String)
+        F: FnMut(&mut Pod, &TestInject) -> (bool, String, String)
     {
-        let mut test_errors: Vec<(&TestInject, String)> = Vec::new();
+        let mut test_errors: Vec<(&TestInject, String, String)> = Vec::new();
         let ok = tests.iter().fold(true, |total, t| {
             let mut pod: Pod = Default::default();
             pod.spec = Some(Default::default());
-            let (result, description) = run_test(&mut pod, t, predicate);
+            pod.spec = Some(Default::default());
+            let (result, test_description, error_message) = run_test(&mut pod, t, predicate);
             if !result {
-                test_errors.push((t, description));
+                test_errors.push((t, test_description, error_message));
             }
             total && result
         });
         if !ok {
             let errors: Vec<String> = test_errors.iter().map(|x|
-                format!("Test {} for {:?} failed, expecting {} but got {}",
-                        x.1, x.0, x.0.result, !x.0.result).to_string()
+                format!("Test {} failed for {:?}, reason: {}",
+                        x.1, x.0, x.2).to_string()
             ).collect();
             panic!("Inject test failed: {}", errors.join("\n"));
         }
@@ -254,7 +264,7 @@ mod tests {
     struct TestInject<'a> {
         labels: Option<Vec<(&'a str, &'a str)>>,
         containers: Vec<&'a str>,
-        result: bool,
+        needs_patching: bool,
     }
 
     fn tests() -> Vec<TestInject<'static>> {
@@ -262,66 +272,67 @@ mod tests {
             TestInject {
                 labels: Some(vec![("appgate-inject", "false")]),
                 containers: vec![],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![]),
                 containers: vec![],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: None,
                 containers: vec![],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec![],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec![],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec!["some-random-service"],
-                result: true,
+                needs_patching: true,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec!["some-random-service-1", "some-random-service-2"],
-                result: true,
+                needs_patching: true,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "false")]),
                 containers: vec!["some-random-service-1", "some-random-service-2"],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec![APPGATE_SIDECAR_NAMES[0], APPGATE_SIDECAR_NAMES[1],
                                  "some-random-service"],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec![APPGATE_SIDECAR_NAMES[1], "some-random-service"],
-                result: false,
+                needs_patching: false,
             },
             TestInject {
                 labels: Some(vec![("appgate-inject", "true")]),
                 containers: vec![APPGATE_SIDECAR_NAMES[0], "some-random-service"],
-                result: false,
+                needs_patching: false,
             }
         ]
     }
 
     #[test]
     fn needs_injection_simple() {
-        let mut predicate = |pod: &mut Pod, test: &TestInject| -> (bool, String) {
-            (test.result == pod.needs_patching(), "Injection Simple Test".to_string())
+        let mut predicate = |pod: &mut Pod, test: &TestInject| -> (bool, String, String) {
+            (test.needs_patching == pod.needs_patching(), "Injection Simple Test".to_string(),
+             format!("{}", AppgatePodDisplay(pod)))
         };
 
         assert_tests(&tests(), &mut predicate)
@@ -329,20 +340,60 @@ mod tests {
 
     #[test]
     fn test_pod_inject_sidecar() {
-        let expected_containers = Some(APPGATE_SIDECAR_NAMES.iter()
-            .map(|n| n.to_string()).collect());
-        let expected_volumes = Some(APPGATE_VOLUME_NAMES.iter()
-            .map(|n| n.to_string()).collect());
-        let appgated_context = load_sidecar_containers()
+        let expected_containers = |vs: &[String]| -> HashSet<String> {
+            let mut xs: Vec<String> = APPGATE_SIDECAR_NAMES.iter().map(|n| n.to_string()).collect();
+            xs.extend_from_slice(vs);
+            HashSet::from_iter(xs.iter().cloned())
+        };
+        let expected_volumes = |vs: &[String]| -> HashSet<String> {
+            let mut xs: Vec<String> = APPGATE_VOLUME_NAMES.iter().map(|n| n.to_string()).collect();
+            xs.extend_from_slice(vs);
+            HashSet::from_iter(xs.iter().cloned())
+        };
+        let appgate_context = load_sidecar_containers()
             .expect("Unable to load the sidecar information");
-        let mut predicate = |pod: &mut Pod, test: &TestInject| -> (bool, String) {
-            let mut r = test.result == pod.needs_patching();
-            if r && test.result {
-                pod.patch_sidecars(&appgated_context);
-                r = r && (pod.sidecar_names() == expected_containers);
-                r = r && (pod.volume_names() == expected_volumes);
+        let mut predicate = |pod: &mut Pod, test: &TestInject| -> (bool, String, String) {
+            let message = format!("Failure for {}", AppgatePodDisplay(pod));
+            let needs_patching = pod.needs_patching();
+            let assert_patch = || -> Result<bool, String> {
+                match pod.patch_sidecars(&appgate_context) {
+                    Ok(Some(patch)) => {
+                        let unpatched_containers = pod.container_names().unwrap_or(vec![]);
+                        let unpatched_volumes = pod.volume_names().unwrap_or(vec![]);
+                        let mut unpatched_pod = serde_json::to_value(pod)
+                            .map_err(|e| format!("Unable to convert POD to value [{}]", e.to_string()))?;
+                        json_patch::patch(&mut unpatched_pod, &patch)
+                            .map_err(|e| format!("Unable to patch POD [{}]", e.to_string()))?;
+                        let patched_pod: Pod = serde_json::from_value(unpatched_pod)
+                            .map_err(|e| format!("Unable to convert patched POD [{}]", e.to_string()))?;
+                        let patched_containers = HashSet::from_iter(patched_pod.container_names()
+                            .unwrap_or(vec![]).iter().cloned());
+                        (patched_containers == expected_containers(&unpatched_containers)).then(|| true)
+                            .ok_or(format!("Wrong containers after patch, got {:?}, expected {:?}", patched_containers,
+                                           expected_containers(&unpatched_containers)))?;
+                        let patched_volumes = HashSet::from_iter(patched_pod.volume_names()
+                            .unwrap_or(vec![]).iter().cloned());
+                        (patched_volumes == expected_volumes(&unpatched_volumes)).then(|| true)
+                            .ok_or(format!("Wrong volumes after patch, got {:?}, expected {:?}", patched_volumes,
+                                           expected_volumes(&unpatched_volumes)))?;
+                        Ok(true)
+                    }
+                    Ok(None) => Err(format!("Patch was expected for {}", AppgatePodDisplay(pod))),
+                    Err(error) =>
+                        Err(format!("Unable to generate patch for {}, {}", AppgatePodDisplay(pod), error.to_string()))
+                }
+            };
+            if test.needs_patching && needs_patching {
+                if let Err(error_message) = assert_patch() {
+                    (false, "Injection Containers Test".to_string(), error_message)
+                } else {
+                    (true, "Injection Containers Test".to_string(), message)
+                }
+            } else if test.needs_patching != needs_patching {
+                (false, "Injection Containers Test".to_string(), message)
+            } else {
+                (true, "Injection Containers Test".to_string(), message)
             }
-            (r, "Injection Containers Test".to_string())
         };
 
         assert_tests(&tests(), &mut predicate)
