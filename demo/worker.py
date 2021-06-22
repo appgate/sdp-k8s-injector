3711997d1da5347993183aabf145604a341b583c
Thomas Cellerier <thomas.cellerier@appgate.com>
2021-06-22 12:02:04 +0200

    Paint it black

--- demo/worker.py
+++ demo/worker.py
@@ -15,7 +15,7 @@ import time
 import urllib.request
 
 
-log = logging.getLogger('demo')
+log = logging.getLogger("demo")
 log.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
@@ -23,36 +23,38 @@ log.addHandler(handler)
 
 
 def fetch_url(url, timeout):
-    log.info('Fetching %s', url)
+    log.info("Fetching %s", url)
     try:
         with urllib.request.urlopen(url, timeout=timeout):
-            return 'ok'
+            return "ok"
     except Exception as e:
         return str(e)
 
 
 def main():
     try:
-        urls = os.environ['DEMO_URLS'].split(',')
+        urls = os.environ["DEMO_URLS"].split(",")
     except KeyError:
-        raise Exception('DEMO_URLS should be set') from None
+        raise Exception("DEMO_URLS should be set") from None
 
-    timeout = float(os.environ.get('DEMO_TIMEOUT', 3.0))
-    update_interval = float(os.environ.get('DEMO_UPDATE_INTERVAL', 2.0))
+    timeout = float(os.environ.get("DEMO_TIMEOUT", 3.0))
+    update_interval = float(os.environ.get("DEMO_UPDATE_INTERVAL", 2.0))
 
     while True:
         results = {}
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(urls)) as pool:
-            for url, result in zip(urls, pool.map(functools.partial(fetch_url, timeout=timeout), urls)):
+            for url, result in zip(
+                urls, pool.map(functools.partial(fetch_url, timeout=timeout), urls)
+            ):
                 results[url] = result
         for url, result in results.items():
-            log.info('%s: %s', url, result)
+            log.info("%s: %s", url, result)
         time.sleep(update_interval)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        log.warning('Interrupted by user.')
+        log.warning("Interrupted by user.")
         sys.exit(1)
