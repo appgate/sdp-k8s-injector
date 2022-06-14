6bd40ec8425e5701ae8d3380c3175f4cc1dd5ce0
Aitor Iturri <aitor.iturri@appgate.com>
2022-06-14 09:52:03 +0200

    GEt ServiceIdentity when requetested

--- src/bin/sdp-service-identity/identity.rs
+++ src/bin/sdp-service-identity/identity.rs
@@ -1,55 +1,101 @@
 mod identity {
-    use futures::StreamExt;
+    use std::borrow::BorrowMut;
+
+    use futures::{StreamExt, channel::mpsc::SendError};
     use k8s_openapi::api::apps::v1::Deployment;
     use kube::{Api, Client, api::ListParams};
     use kube_runtime::watcher;
     use kube_runtime::watcher::Event;
-    use log::info;
+    use log::{info, error, warn};
     use tokio::sync::mpsc::{channel, Receiver, Sender};
 
+    #[derive(Clone)]
+    pub struct ServiceIdentity {
+        secret: String,
+        field: String
+    }
+
     /// Messages exchanged between different components
-    pub enum IdentityMessage {
+    pub enum IdentityMessageRequest {
         /// Message used to request a new user
         RequestIdentity {
             service_name: String,
             service_ns: String,
+            channel: Sender<IdentityMessageResponse> 
         },
-        /// Message used to send a new user
-        NewIdentity { secret_name: String },
     }
 
-    pub struct IdentityManager;
+    pub enum IdentityMessageResponse {
+        /// Message used to send a new user
+        NewIdentity(ServiceIdentity),
+        IdentityUnavailable,
+    }
 
-    async fn identity_manager(mut rx: Receiver<IdentityMessage>) -> () {
-        while let Some(msg) = rx.recv().await {
-            match msg {
-                IdentityMessage::RequestIdentity {
-                    service_name,
-                    service_ns,
-                } => {
-                    info!(
-                        "New user requested for service {}[{}]",
-                        service_name, service_ns
-                    );
-                }
-                _ => {
-                    info!("Unknown message");
+    pub struct IdentityManager {
+        pool_size: u8,
+        pool: Vec<ServiceIdentity>,
+        idx: usize
+    }
+
+    impl IdentityManager {
+        fn new() -> IdentityManager {
+            IdentityManager { 
+                pool_size: 30, 
+                pool: vec!(),
+                idx: 0,
+             }
+        }
+
+        fn next_identity(&mut self) -> Option<ServiceIdentity> {
+            let identity = self.pool.get(self.idx).map(|id| id.clone());
+            if let Some(_) = identity {
+                self.idx += 1;
+            }
+            identity
+        }
+
+        async fn run(&mut self, mut rx: Receiver<IdentityMessageRequest>) -> () {
+            while let Some(msg) = rx.recv().await {
+                match msg {
+                    IdentityMessageRequest::RequestIdentity {
+                        service_name,
+                        service_ns,
+                        channel,
+                    } => {
+                        info!(
+                            "New user requested for service {}[{}]",
+                            service_name, service_ns
+                        );
+                        let event = self.next_identity()
+                        .map(|id|IdentityMessageResponse::NewIdentity(id))
+                        .unwrap_or_else(|| {
+                            warn!("Unable to get next ServiceIdentity");
+                            IdentityMessageResponse::IdentityUnavailable
+                        });
+                        if let Err(err) = channel.send(event).await {
+                            error!("Found error when notifying ServiceIdentify: {}", err);
+                        }
+                    }
                 }
             }
         }
-    }
 
-    impl IdentityManager {  
-        async fn run() -> Sender<IdentityMessage> {
-            let (tx, mut rx) = channel::<IdentityMessage>(50);
-            tokio::spawn(async move {
-                identity_manager(rx).await;
-            });
-            tx
+        async fn initialize(&mut self) -> () {
+            ()
         }
     }
 
+    async fn run() -> Sender<IdentityMessageRequest> {
+        let (tx, mut rx) = channel::<IdentityMessageRequest>(50);
+        let mut identity_manager = IdentityManager::new();
+        tokio::spawn(async move {
+            identity_manager.borrow_mut().run(rx).await;
+        });
+        tx
+    }
+
     pub struct DeploymentWatcher;
+
     async fn watch_deployments(client: Client) -> () {
         let deployments_api: Api<Deployment> = Api::all(client);
         watcher(deployments_api, ListParams::default())
