5bf96cc2648619abac9a726c6e0e5487fd856642
Aitor Iturri <aitor.iturri@appgate.com>
2022-06-14 11:48:14 +0200

    Add new users into the pool as needed

--- src/bin/sdp-service-identity/identity.rs
+++ src/bin/sdp-service-identity/identity.rs
@@ -1,124 +1,180 @@
-mod identity {
-    use std::{borrow::BorrowMut, collections::VecDeque};
+use std::{borrow::BorrowMut, collections::VecDeque};
 
-    use futures::StreamExt;
-    use k8s_openapi::api::apps::v1::Deployment;
-    use kube::{api::ListParams, Api, Client};
-    use kube_runtime::watcher;
-    use kube_runtime::watcher::Event;
-    use log::{error, info, warn};
-    use tokio::sync::mpsc::{channel, Receiver, Sender};
+use futures::StreamExt;
+use k8s_openapi::api::apps::v1::Deployment;
+use kube::{api::ListParams, Api, Client};
+use kube_runtime::watcher;
+use kube_runtime::watcher::Event;
+use log::{error, info, warn};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
-    #[derive(Clone)]
-    pub struct ServiceIdentity {
-        secret: String,
-        field: String,
-    }
+pub use crate::sdp;
 
-    /// Messages exchanged between different components
-    pub enum IdentityMessageRequest {
-        /// Message used to request a new user
-        RequestIdentity {
-            service_name: String,
-            service_ns: String,
-            channel: Sender<IdentityMessageResponse>,
-        },
-    }
+use self::sdp::Credentials;
 
-    pub enum IdentityMessageResponse {
-        /// Message used to send a new user
-        NewIdentity(ServiceIdentity),
-        IdentityUnavailable,
-    }
+#[derive(Clone)]
+pub struct ServiceIdentity {
+    secret: String,
+    field: String,
+}
 
-    pub struct IdentityManager {
-        pool: VecDeque<ServiceIdentity>,
-    }
+/// Messages exchanged between different components
+pub enum IdentityMessageRequest {
+    /// Message used to request a new user
+    RequestIdentity {
+        service_name: String,
+        service_ns: String,
+        channel: Sender<IdentityMessageResponse>,
+    },
+    CredentialsCreated(Credentials),
+}
 
-    impl IdentityManager {
-        fn new() -> IdentityManager {
-            IdentityManager {
-                pool: VecDeque::with_capacity(30),
-            }
+pub enum IdentityMessageResponse {
+    /// Message used to send a new user
+    NewIdentity(ServiceIdentity),
+    IdentityUnavailable,
+}
+
+pub enum IdentityCreatorMessage {
+    /// Message used to send a new user
+    CreateIdentity,
+    DeleteIdentity,
+}
+
+pub struct IdentityManager {
+    pool: VecDeque<ServiceIdentity>,
+}
+
+impl IdentityManager {
+    fn new() -> IdentityManager {
+        IdentityManager {
+            pool: VecDeque::with_capacity(30),
         }
+    }
 
-        fn next_identity(&mut self) -> Option<ServiceIdentity> {
-            self.pool.pop_front().map(|id| id.clone())
-        }
+    fn next_identity(&mut self) -> Option<ServiceIdentity> {
+        self.pool.pop_front().map(|id| id.clone())
+    }
 
-        async fn run(&mut self, mut rx: Receiver<IdentityMessageRequest>) -> () {
-            while let Some(msg) = rx.recv().await {
-                match msg {
-                    IdentityMessageRequest::RequestIdentity {
-                        service_name,
-                        service_ns,
-                        channel,
-                    } => {
-                        info!(
-                            "New user requested for service {}[{}]",
-                            service_name, service_ns
-                        );
-                        let event = self
-                            .next_identity()
-                            .map(|id| IdentityMessageResponse::NewIdentity(id))
-                            .unwrap_or_else(|| {
-                                warn!("Unable to get next ServiceIdentity");
-                                IdentityMessageResponse::IdentityUnavailable
-                            });
-                        if let Err(err) = channel.send(event).await {
+    async fn run(&mut self, mut rx: Receiver<IdentityMessageRequest>) -> () {
+        while let Some(msg) = rx.recv().await {
+            match msg {
+                IdentityMessageRequest::RequestIdentity {
+                    service_name,
+                    service_ns,
+                    channel,
+                } => {
+                    info!(
+                        "New user requested for service {}[{}]",
+                        service_name, service_ns
+                    );
+                    let event = self
+                        .next_identity()
+                        .map(|id| IdentityMessageResponse::NewIdentity(id))
+                        .unwrap_or_else(|| {
+                            warn!("Unable to get next ServiceIdentity");
+                            IdentityMessageResponse::IdentityUnavailable
+                        });
+                    match channel.send(event).await {
+                        Ok(_) => {
+                            info!("New ServiceIdentity dispatched");
+                        }
+                        Err(err) => {
                             error!("Found error when notifying ServiceIdentify: {}", err);
                         }
-                    }
+                    };
+                }
+                IdentityMessageRequest::CredentialsCreated(credentials) => {
+                    // TODO: Convert Crendetials => ServiceIdentity
+                    self.pool.push_back(ServiceIdentity {
+                        secret: "THESECRET".to_string(),
+                        field: "THEFIELD".to_string(),
+                    });
                 }
             }
         }
-
-        async fn initialize(&mut self) -> () {
-            ()
-        }
     }
 
-    async fn run() -> Sender<IdentityMessageRequest> {
-        let (tx, mut rx) = channel::<IdentityMessageRequest>(50);
-        let mut identity_manager = IdentityManager::new();
-        tokio::spawn(async move {
-            identity_manager.borrow_mut().run(rx).await;
-        });
-        tx
+    async fn initialize(&mut self) -> () {
+        ()
     }
 
-    pub struct DeploymentWatcher;
-
-    async fn watch_deployments(client: Client) -> () {
-        let deployments_api: Api<Deployment> = Api::all(client);
-        watcher(deployments_api, ListParams::default())
-            .for_each_concurrent(5, |res| async move {
-                match res {
-                    Ok(Event::Restarted(deployments)) => {
-                        println!("Watcher restarted");
-                    }
-                    Ok(Event::Applied(deployment)) => {
-                        println!("New deployment or modified deployment");
-                    }
-                    Ok(Event::Deleted(deployment)) => {
-                        println!("Deleted deployment");
+    async fn identity_creator(
+        system: sdp::System,
+        mut rx: Receiver<IdentityCreatorMessage>,
+        mut tx: Sender<IdentityMessageRequest>,
+    ) -> () {
+        while let Some(message) = rx.recv().await {
+            match message {
+                CreateIdentity => {
+                    match system.create_user().await {
+                        Ok(credentials) => {
+                            if let Err(err) = tx
+                                .send(IdentityMessageRequest::CredentialsCreated(credentials))
+                                .await
+                            {
+                                error!("Error notifying Credentials: {}", err);
+                                // TODO: Try later, credentials are already created
+                            }
+                        }
+                        Err(err) => {
+                            error!("Error creating new Credentials: {}", err);
+                        }
+                    };
+                }
+                DeleteIdentity => match system.delete_user().await {
+                    Ok(credentials) => {
+                        info!("Credentials deleted!");
                     }
                     Err(err) => {
-                        println!("Some error")
+                        error!("Unable to delete credentials");
                     }
-                }
-            })
-            .await
-    }
-
-    impl DeploymentWatcher {
-        async fn run() -> () {
-            tokio::spawn(async move {
-                let client = Client::try_default()
-                    .await
-                    .expect("Unable to create K8S client");
-                watch_deployments(client);
-            });
+                },
+            }
         }
+    }
+}
+
+async fn run() -> Sender<IdentityMessageRequest> {
+    let (tx, mut rx) = channel::<IdentityMessageRequest>(50);
+    let mut identity_manager = IdentityManager::new();
+    tokio::spawn(async move {
+        identity_manager.borrow_mut().run(rx).await;
+    });
+    tx
+}
+
+pub struct DeploymentWatcher;
+
+async fn watch_deployments(client: Client) -> () {
+    let deployments_api: Api<Deployment> = Api::all(client);
+    watcher(deployments_api, ListParams::default())
+        .for_each_concurrent(5, |res| async move {
+            match res {
+                Ok(Event::Restarted(deployments)) => {
+                    println!("Watcher restarted");
+                }
+                Ok(Event::Applied(deployment)) => {
+                    println!("New deployment or modified deployment");
+                }
+                Ok(Event::Deleted(deployment)) => {
+                    println!("Deleted deployment");
+                }
+                Err(err) => {
+                    println!("Some error")
+                }
+            }
+        })
+        .await
+}
+
+impl DeploymentWatcher {
+    async fn run() -> () {
+        tokio::spawn(async move {
+            let client = Client::try_default()
+                .await
+                .expect("Unable to create K8S client");
+            watch_deployments(client);
+        });
     }
 }
--- src/bin/sdp-service-identity/sdp.rs
+++ src/bin/sdp-service-identity/sdp.rs
@@ -1,97 +1,97 @@
-mod sdp {
-    use reqwest::header::HeaderMap;
-    use reqwest::{Client, Error as RError, Url};
-    use serde::{Deserialize, Serialize};
+use http::Error;
+use reqwest::header::HeaderMap;
+use reqwest::{Client, Error as RError, Url};
+use serde::{Deserialize, Serialize};
 
-    #[derive(Deserialize, Serialize, Debug)]
-    #[serde(rename_all = "camelCase")]
-    pub struct LoginUser {
-        name: String,
-        need_two_factor_auth: bool,
-        can_access_audit_logs: bool,
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LoginUser {
+    name: String,
+    need_two_factor_auth: bool,
+    can_access_audit_logs: bool,
+}
+
+type Token = String;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Login {
+    user: LoginUser,
+    token: Token,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Credentials {
+    pub username: String,
+    password: String,
+    provider_name: String,
+    device_id: Option<String>,
+}
+
+#[derive(Clone)]
+struct SystemConfig {
+    hosts: Vec<Url>,
+    api_version: Option<String>,
+}
+
+pub struct SystemBuilder {
+    config: SystemConfig,
+}
+
+pub struct System {
+    config: SystemConfig,
+    client: Client,
+}
+
+impl SystemBuilder {
+    fn build(&self) -> Result<System, String> {
+        let mut hm = HeaderMap::new();
+        Client::builder()
+            .default_headers(hm)
+            .build()
+            .map_err(|e| format!("Unable to crate the client: {:?}", e))
+            .map(|c| System {
+                config: self.config.clone(),
+                client: c,
+            })
+    }
+}
+
+impl System {
+    fn headers() -> HeaderMap {
+        let mut hm = HeaderMap::new();
+        hm
     }
 
-    type Token = String;
-
-    #[derive(Deserialize, Serialize, Debug)]
-    pub struct Login {
-        user: LoginUser,
-        token: Token,
+    /// /login
+    /// Remove the clone!
+    async fn login(&self, creds: Credentials) -> Result<Login, RError> {
+        let resp = self
+            .client
+            .post(self.config.hosts[0].clone())
+            .json(&creds)
+            .send()
+            .await?;
+        resp.json::<Login>().await
     }
 
-    #[derive(Serialize, Deserialize, Debug)]
-    #[serde(rename_all = "camelCase")]
-    struct Credentials {
-        username: String,
-        password: String,
-        provider_name: String,
-        device_id: String,
+    /// GET /service-users
+    pub async fn get_users(&self, login: Login) -> Result<Vec<Credentials>, RError> {
+        unimplemented!();
     }
 
-    struct SystemConfig {
-        hosts: Vec<Url>,
-        api_version: Option<String>,
+    /// GET /service-users-id
+    pub async fn get_user(&self) -> Result<Credentials, RError> {
+        unimplemented!();
     }
 
-    struct SystemBuilder {
-        config: SystemConfig,
+    /// POST /service-users-id
+    pub async fn create_user(&self) -> Result<Credentials, RError> {
+        unimplemented!();
     }
 
-    struct System<'a> {
-        config: &'a SystemConfig,
-        client: Client,
-    }
-
-    impl SystemBuilder {
-        fn build(&self) -> Result<System, String> {
-            let mut hm = HeaderMap::new();
-            Client::builder()
-                .default_headers(hm)
-                .build()
-                .map_err(|e| format!("Unable to crate the client: {:?}", e))
-                .map(|c| System {
-                    config: &self.config,
-                    client: c,
-                })
-        }
-    }
-
-    impl System<'static> {
-        fn headers() -> HeaderMap {
-            let mut hm = HeaderMap::new();
-            hm
-        }
-
-        /// /login
-        /// Remove the clone!
-        async fn login(&self, creds: Credentials) -> Result<Login, RError> {
-            let resp = self
-                .client
-                .post(self.config.hosts[0].clone())
-                .json(&creds)
-                .send()
-                .await?;
-            resp.json::<Login>().await
-        }
-
-        /// GET /service-users
-        async fn get_users(&self, login: Login) -> Result<(), RError> {
-            unimplemented!();
-        }
-
-        /// GET /service-users-id
-        async fn get_user(&self) -> () {
-            unimplemented!();
-        }
-
-        /// POST /service-users-id
-        async fn create_user(&self) -> () {
-            unimplemented!();
-        }
-
-        /// DELETE /service-user-id
-        async fn delete_user(&self) -> () {
-            unimplemented!();
-        }
+    /// DELETE /service-user-id
+    pub async fn delete_user(&self) -> Result<Credentials, RError> {
+        unimplemented!();
     }
 }
