0bb0a3789d3ac5428dcd16ed05141beaec6ba7e6
Masaya Ando <masaya.ando@appgate.com>
2022-08-18 10:05:19 -0400

    Skeleton for sdp-device-id-service

--- sdp-device-id-service/src/main.rs
+++ sdp-device-id-service/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+enum DeviceIdCommands {
+    /// Runs the DeviceId Service
+    Run,
+}
+
+#[derive(Debug, Parser)]
+#[clap(name = "sdp-device-id-service")]
+struct DeviceIdService {
+    #[clap(subcommand)]
+    command: DeviceIdCommands,
+}
+
+#[tokio::main]
+async fn main() -> () {
+    env_logger::init();
+    let args = DeviceIdService::parse();
+    match args.command {
+        DeviceIdCommands::Run => {
+            println!("Hello World!")
+        }
+    }
 }
