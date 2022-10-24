acbc39bf06762990b911a0ca2be39139d76a5c6c
Masaya Ando <masaya.ando@appgate.com>
2022-10-24 12:12:18 -0400

    Use const_format for formatting annotation constatnts

--- sdp-common/src/annotations.rs
+++ sdp-common/src/annotations.rs
@@ -1,22 +1,13 @@
-pub const SDP_INJECTOR_ANNOTATION_STRATEGY: &str =
-    concat!("k8s.appgate.com", "~1", "sdp-injector", ".", "strategy");
-pub const SDP_INJECTOR_ANNOTATION_ENABLED: &str =
-    concat!("k8s.appgate.com", "~1", "sdp-injector", ".", "enabled");
-pub const SDP_INJECTOR_ANNOTATION_CLIENT_VERSION: &str = concat!(
-    "k8s.appgate.com",
-    "~1",
-    "sdp-injector",
-    ".",
-    "client-version"
-);
-pub const SDP_INJECTOR_ANNOTATION_DISABLE_INIT_CONTAINERS: &str = concat!(
-    "k8s.appgate.com",
-    "~1",
-    "sdp-injector",
-    ".",
-    "disable-init-containers"
-);
-pub const SDP_ANNOTATION_CLIENT_CONFIG: &str = "sdp-injector-client-config";
-pub const SDP_ANNOTATION_CLIENT_SECRETS: &str = "sdp-injector-client-secrets";
-pub const SDP_ANNOTATION_CLIENT_DEVICE_ID: &str = "sdp-injector-client-device-id";
-pub const SDP_ANNOTATION_DNS_SEARCHES: &str = "sdp-injector-dns-searches";
+use const_format::formatcp;
+
+const DOMAIN_ANNOTATION: &str = "k8s.appgate.com";
+const COMPONENT_ANNOTATION: &str = "sdp-injector";
+
+pub const SDP_INJECTOR_ANNOTATION_STRATEGY: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.strategy");
+pub const SDP_INJECTOR_ANNOTATION_ENABLED: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.enabled");
+pub const SDP_INJECTOR_ANNOTATION_CLIENT_VERSION: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-version");
+pub const SDP_INJECTOR_ANNOTATION_DISABLE_INIT_CONTAINERS: &str  = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.disable-init-containers");
+pub const SDP_ANNOTATION_CLIENT_CONFIG: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-config");
+pub const SDP_ANNOTATION_CLIENT_SECRETS: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-secrets");
+pub const SDP_ANNOTATION_CLIENT_DEVICE_ID: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.device-id");
+pub const SDP_ANNOTATION_DNS_SEARCHES: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.dns-searches");
