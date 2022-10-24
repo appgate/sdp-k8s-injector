b3df6ec9468694fbaa2a3b1022c9814711a2abd2
Masaya Ando <masaya.ando@appgate.com>
2022-10-24 12:28:38 -0400

    Use appgate_annotate macro

--- sdp-common/src/annotations.rs
+++ sdp-common/src/annotations.rs
@@ -3,11 +3,23 @@ use const_format::formatcp;
 const DOMAIN_ANNOTATION: &str = "k8s.appgate.com";
 const COMPONENT_ANNOTATION: &str = "sdp-injector";
 
-pub const SDP_INJECTOR_ANNOTATION_STRATEGY: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.strategy");
-pub const SDP_INJECTOR_ANNOTATION_ENABLED: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.enabled");
-pub const SDP_INJECTOR_ANNOTATION_CLIENT_VERSION: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-version");
-pub const SDP_INJECTOR_ANNOTATION_DISABLE_INIT_CONTAINERS: &str  = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.disable-init-containers");
-pub const SDP_ANNOTATION_CLIENT_CONFIG: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-config");
-pub const SDP_ANNOTATION_CLIENT_SECRETS: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.client-secrets");
-pub const SDP_ANNOTATION_CLIENT_DEVICE_ID: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.device-id");
-pub const SDP_ANNOTATION_DNS_SEARCHES: &str = formatcp!("{DOMAIN_ANNOTATION}~1{COMPONENT_ANNOTATION}.dns-searches");
+macro_rules! appgate_annotate {
+    ($annotation:literal) => {{
+        formatcp!(
+            "{}~1{}.{}",
+            DOMAIN_ANNOTATION,
+            COMPONENT_ANNOTATION,
+            $annotation
+        )
+    }};
+}
+
+pub const SDP_INJECTOR_ANNOTATION_STRATEGY: &str = appgate_annotate!("strategy");
+pub const SDP_INJECTOR_ANNOTATION_ENABLED: &str = appgate_annotate!("enabled");
+pub const SDP_INJECTOR_ANNOTATION_CLIENT_VERSION: &str = appgate_annotate!("client-version");
+pub const SDP_INJECTOR_ANNOTATION_DISABLE_INIT_CONTAINERS: &str =
+    appgate_annotate!("disable-init-containers");
+pub const SDP_ANNOTATION_CLIENT_CONFIG: &str = appgate_annotate!("client-config");
+pub const SDP_ANNOTATION_CLIENT_SECRETS: &str = appgate_annotate!("client-secrets");
+pub const SDP_ANNOTATION_CLIENT_DEVICE_ID: &str = appgate_annotate!("device-id");
+pub const SDP_ANNOTATION_DNS_SEARCHES: &str = appgate_annotate!("dns-searches");
