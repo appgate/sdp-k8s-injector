a4f6659b13d392277ceb3c8a2a7c9184738540f6
Masaya Ando <masaya.ando@appgate.com>
2022-11-02 14:36:53 -0400

    Reduce retry to 15

--- sdp-common/src/constants.rs
+++ sdp-common/src/constants.rs
@@ -2,4 +2,4 @@ pub const SDP_IDP_NAME: &str = "service";
 pub const IDENTITY_MANAGER_SECRET_NAME: &str = "sdp-identity-service-creds";
 pub const SDP_CLUSTER_ID_ENV: &str = "SDP_CLUSTER_ID";
 pub const SDP_DEFAULT_CLIENT_VERSION_ENV: &str = "SDP_DEFAULT_CLIENT_VERSION";
-pub const MAX_PATCH_ATTEMPTS: u8 = 254;
+pub const MAX_PATCH_ATTEMPTS: u8 = 15;
